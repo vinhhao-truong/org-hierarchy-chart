ce404132ea66bb84a4e955dc01941839b21c0958
arnold-tr <vinhhao.truong.52@gmail.com>
2023-10-09 23:31:39 +1100

    Add up down arrow for desktop hiechart

--- components/pages/Home/HieChart/DesktopHieChart/index.tsx
+++ components/pages/Home/HieChart/DesktopHieChart/index.tsx
@@ -4,21 +4,26 @@ import { useGetOrgStructureQuery } from "@/redux/services/api";
 import useTheme from "@mui/material/styles/useTheme";
 import Box from "@mui/material/Box";
 import React from "react";
-import Image from "next/image";
 import { flex } from "@/utils/get/getSxMUI";
 import ChartColumn from "./ChartColumn";
-import LoadingAnimation from "../LoadingAnimation";
+import Stack from "@mui/material/Stack";
+import UpIcon from "@mui/icons-material/KeyboardArrowUpRounded";
+import DownIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import { SxProps } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 const DesktopHieChart = () => {
-  const {
-    data: employeeList,
-    isLoading,
-    isFetching,
-  } = useGetOrgStructureQuery();
+  const { data: employeeList } = useGetOrgStructureQuery();
 
-  const isListLoading: boolean = isLoading || isFetching;
+  const arrowSx = (unavailable: boolean): SxProps => {
+    return {
+      color: unavailable ? "#bdbdbd" : "#00000",
+      cursor: unavailable ? "none" : "pointer",
+    };
+  };
 
   const theme = useTheme();
+  const router = useRouter();
 
   const topLevel = employeeList?.filter(({ level }) => level === 1);
 
@@ -32,8 +37,38 @@ const DesktopHieChart = () => {
       }}
     >
       {topLevel?.map((employee, idx) => {
+        const next = topLevel[idx + 1]?.id;
+        const prev = topLevel[idx - 1]?.id;
+
         return (
-          <Box key={`hie-col-${idx}`} sx={{ py: 4, display: "relative" }}>
+          <Box
+            id={`top-${employee.id}-section`}
+            key={`hie-col-${idx}`}
+            sx={{
+              ...flex("row", "space-between"),
+              py: 4,
+              position: "relative",
+            }}
+          >
+            <Stack
+              sx={{
+                position: "relative",
+                width: "fit-content",
+                fontSize: "48px",
+                gap: 3,
+              }}
+            >
+              <UpIcon
+                onClick={() => prev && router.push(`/#top-${prev}-section`)}
+                sx={arrowSx(!prev)}
+                fontSize="inherit"
+              />
+              <DownIcon
+                onClick={() => next && router.push(`/#top-${next}-section`)}
+                sx={arrowSx(!next)}
+                fontSize="inherit"
+              />
+            </Stack>
             <ChartColumn
               numberOfSameLvl={topLevel.length}
               topEmployee={employee}
